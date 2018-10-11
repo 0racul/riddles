977c11655aa490d60f18a670da35259ed0e34b97
0racul <magdeevd@yandex.ru>
2018-10-11 16:29:47 +0700

    http://www.pythonchallenge.com/pc/def/equality.html
    допетрить до конца, хуй пойми что за ересь

--- soup_file.py
+++ soup_file.py
@@ -1,22 +1,36 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup, Comment
+import re
 import collections
 
-page = urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
+page = urlopen("http://www.pythonchallenge.com/pc/def/equality.html")
 
 
 soup = BeautifulSoup(page, "html.parser")
 
 comments = soup.findAll(text=lambda text: isinstance(text, Comment))
 
-comments.remove("\nfind rare characters in the mess below:\n")
+result_mid = re.findall("[A-Z]{3}[a-z][A-Z]{3}", comments[0])
+
+#n = 0
+
+#list_dohuya = []
+
+#for abvgd in result_mid:
+
+#    abvgd_temp = abvgd[3]
+
+#    list_dohuya.append(abvgd_temp)
 
 
-result = collections.Counter(comments[0])
+#comments.remove("\nfind rare characters in the mess below:\n")
 
 
-result = dict((k, v) for k, v in result.items() if v == 1)
+#result = collections.Counter(comments[0])
 
 
-print(result)
+#result = dict((k, v) for k, v in result.items() if v == 1)
+
+
+print(result_mid)
 
