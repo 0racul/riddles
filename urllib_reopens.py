06fb63bc9314a16af27d55be85b05e3d2448433e
0racul <magdeevd@yandex.ru>
2018-10-18 15:01:55 +0700

    http://www.pythonchallenge.com/pc/def/peak.html L4 done

--- urllib_reopens.py
+++ urllib_reopens.py
@@ -10,6 +10,8 @@ line1 = soup.find_all('a')
 
 needed_line = 'http://www.pythonchallenge.com/pc/def/' + line1[0].get('href')
 
+#needed_line = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=82682'
+
 tushka = re.findall(r'\D+', needed_line)
 
 count = 0
@@ -17,21 +19,29 @@ current_number = ''
 while True:
 
     current_url = str(urllib.request.urlopen(needed_line).read())
-    temp_number = re.search(r'the next nothing is \d+', current_url).group()
-    if temp_number is not None:
-        current_number = re.search(r'\d+', temp_number).group()
-        current_url1 = str(tushka[0] + current_number)
+
+    if len(re.findall(r'\d+', current_url)) == 1:
+
+        temp_number = re.findall(r'\d+', current_url)
+        current_url1 = str(tushka[0] + temp_number[0])
         needed_line = current_url1
 
-    elif temp_number is None:
+    elif re.search(r'\d+', current_url) is None:
 
-        current_number = re.search(r'\d+', needed_line).group()
-        current_url1 = str(tushka[0] + str(int(current_number)/2))
+        temp_number = re.search(r'\d+', needed_line).group()
+        current_url1 = str(tushka[0] + str(int(temp_number)/2))
+        needed_line = current_url1
+
+    elif len(re.findall(r'\d+', current_url)) > 1:
+
+        temp_temp = re.findall(r'the next nothing is \d+', current_url)
+        temp_number = re.findall(r'\d+', temp_temp[0])
+        current_url1 = str(tushka[0] + temp_number[0])
         needed_line = current_url1
 
     count = count + 1
     print(count)
-    print(current_number)
+    print(current_url1)
     print(current_url)
 
 print(current_url1)
